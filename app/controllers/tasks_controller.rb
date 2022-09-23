a7956548eb730c16cca3961813dc7993fd83f38a
AngelFBI <angel.bejarano@enroutesystems.com>
2022-09-23 17:23:30 -0500

    Added edit and update action
    - Added edit and update to the set_task before action
    - Added task_params method in order to avoid repeated code
    - Fixed create action with the new task_params method
    - Update action also create a new task in order to keep track of the history of modification

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,8 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [ :show, :edit ]
+  before_action :set_task, only: [ :show, :edit, :update ]
 
   def index
-    @tasks = Task.all
+    @tasks = Task.all.current_tasks
   end
 
   def show
@@ -13,7 +13,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(params.require(:task).permit(:title, :details))
+    @task = Task.new(task_params)
     if @task.save
       flash[:success] = 'Task was created successfully'
       redirect_to tasks_path
@@ -26,6 +26,16 @@ class TasksController < ApplicationController
   end
 
   def update
+    @new_task = Task.new(task_params)
+    @new_task.created_at = @task.updated_at
+    @new_task.current_task_id = @task.id
+    if @task.update(task_params)
+      flash[:success] = 'Task was created successfully'
+      @new_task.save
+      redirect_to @task
+    else
+      render 'new'
+    end
   end
 
   def destroy
@@ -34,5 +44,9 @@ class TasksController < ApplicationController
   private
   def set_task
     @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
