0f5180bfae713cfaf8b53f377a80018a18ab7050
AngelFBI <angel.bejarano@enroutesystems.com>
2022-09-23 17:20:03 -0500

    Added Relationships
    - Added edited_tasks in order to track records from their current_task
    - Added current_task foreing key pointing to the same table
    - Added scope to display just the current tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,9 @@
 class Task < ApplicationRecord
+  has_many :edited_tasks, class_name: "Task",
+    foreign_key: "current_task_id"
+  belongs_to :current_task, class_name: "Task", optional: true
+
+  scope :current_tasks, ->{ where(current_task_id: nil) }
   validates :title, presence: true
   validates :details, length: { maximum: 500 }
 end
