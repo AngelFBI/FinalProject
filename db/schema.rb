dd97b6ece21c44f6a34b99321be4d44c08971eb5
AngelFBI <angel.bejarano@enroutesystems.com>
2022-09-26 16:35:24 -0500

    Fixed display of rich text details in show and index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_26_185736) do
+ActiveRecord::Schema.define(version: 2022_09_26_212750) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -52,7 +52,6 @@ ActiveRecord::Schema.define(version: 2022_09_26_185736) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.text "details"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
