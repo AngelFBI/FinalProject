c97f2462737f023d458c882fd4609c119941803d
AngelFBI <angel.bejarano@enroutesystems.com>
2022-09-29 16:20:06 -0500

    Added Method to calculate doing_time
    - Added time in hours

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,17 @@ class Task < ApplicationRecord
   has_rich_text :details
 
   validates :title, presence: true
+
+  def doing_time
+    doing_time_in_seconds / 1.hour if doing_time_in_seconds
+  end
+
+  private
+  def doing_time_in_seconds
+    begin
+      (finished_at - started_at).to_i
+    rescue Exception => e
+      nil
+    end
+  end
 end
