59483c38ed22fe2c8d441e7b6e4c8602f60818ed
AngelFBI <angel.bejarano@enroutesystems.com>
2022-10-04 11:49:42 -0500

    Added list index
    - Created lists controller
      - Added action index
    - Added styles to display lists cards correctly
    - Created index view
    - Added route to list index
    - Added resources lists in routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   resources :tasks
+  resources :lists
   get 'tasks/:id/justification', to: 'tasks/justification#edit',
                                  as: 'justification'
 end
