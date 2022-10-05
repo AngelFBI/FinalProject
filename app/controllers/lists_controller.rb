9f69e74c901dee63497b3bf22995fa23537a1b61
AngelFBI <angel.bejarano@enroutesystems.com>
2022-10-05 15:41:50 -0500

    Added actions new, create, edit, update, destroy to lists controller
    - Added privates methods: list_params and set_list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,49 @@
 # frozen_string_literal: true
 
 class ListsController < ApplicationController
+
+  before_action :set_list, only: %i[edit update destroy]
   def index
     @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      flash[:success] = 'List was created successfully'
+      redirect_to lists_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @list.update(list_params)
+      flash[:success] = 'List was updated successfully'
+      redirect_to lists_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, :color, :priority)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
   end
 end
