d6a1bacfeae444073a00b9ecdb7eb7797c8a1148
AngelFBI <angel.bejarano@enroutesystems.com>
2022-10-05 18:27:05 -0500

    Added relationship between task and task history
    - Added before save callback to create task history

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,9 +3,12 @@
 class Task < ApplicationRecord
   has_rich_text :details
   belongs_to :list
+  has_many :task_histories
 
   validates :title, presence: true
   validates :list_id, presence: true
+
+  before_save :add_task_history
 
   def doing_time
     doing_time_in_seconds / 1.hour if doing_time_in_seconds
@@ -17,5 +20,9 @@ class Task < ApplicationRecord
     (finished_at - started_at).to_i
   rescue StandardError
     nil
+  end
+
+  def add_task_history
+    TaskHistory.create(list: list.name, task_id: id) unless list_id == list_id_was
   end
 end
