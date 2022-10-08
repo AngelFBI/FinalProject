5cbffcb94d3656257945e299a005ae23f530985a
AngelFBI <angel.bejarano@enroutesystems.com>
2022-10-07 19:04:44 -0500

    Added seeds for boards

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,25 +8,36 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+boards = [
+  { name: 'board 1', visibility: :public },
+  { name: 'board 2', visibility: :public },
+  { name: 'board 3', visibility: :private }
+]
+
+Board.create(boards)
+
 lists = [
   {
     name: 'List 1',
     color: 'success',
-    priority: 'Low'
+    priority: 'Low',
   },
   {
     name: 'List 2',
     color: 'danger',
-    priority: 'Medium'
+    priority: 'Medium',
   },
   {
     name: 'List 3',
     color: 'info',
-    priority: 'High'
+    priority: 'High',
   }
 ]
 
-List.create(lists)
+(1..3).each do |i|
+  lists = lists.each { |list| list[:board_id] = i }
+  List.create(lists)
+end
 
 tasks = [
   {
@@ -34,27 +45,26 @@ tasks = [
     details: 'A very well decriptive first task',
     started_at: Time.zone.now - 3.days,
     finished_at: Time.zone.now,
-    justification: 'Was a complicated task',
-    list_id: List.first.id
+    justification: 'Was a complicated task'
   },
   {
     title: 'Task 2',
     details: '<ol><li>first this</li><li>Then this</li></ol>',
     started_at: Time.zone.now - 3.days,
-    finished_at: Time.zone.now,
-    list_id: List.first.id
+    finished_at: Time.zone.now
   },
   {
     title: 'Task 3',
     details: '<ul><li>do this</li><li>do also this</li></ul>',
-    started_at: Time.zone.now - 3.days,
-    list_id: List.second.id
+    started_at: Time.zone.now - 3.days
   },
   {
     title: 'Task 4',
-    details: 'You can do it, I believe in you',
-    list_id: List.last.id
+    details: 'You can do it, I believe in you'
   }
 ]
 
-Task.create(tasks)
+(1..9).each do |i|
+  tasks = tasks.each { |task| task[:list_id] = i }
+  Task.create(tasks)
+end
